d94fa3c23bf8c5159944bd295df29b01c6d09d06
nyandoragora <kou.chocobo@gmail.com>
2025-04-26 18:55:21 +0900

    Add files via upload

---
+++ kadai_018/KatoIchiro_Chapter18.java
@@ -0,0 +1,13 @@
+package kadai_018;
+
+public class KatoIchiro_Chapter18 extends Kato_Chapter18{
+
+	public void eachIntroduce() {
+		System.out.println("好きな食べ物はりんごです");
+	}
+	
+	public void setName(String name){
+		this.givenName = name;
+	}
+
+}
